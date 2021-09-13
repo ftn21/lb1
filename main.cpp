fafe823815d1f87a7efbff4f0f7d2b76d92f4b52
st1nkybom <akulakova29@gmail.com>
2021-09-13 10:14:10 +0300

    +

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-#include <string.h> //memset
+#include <string.h> // memset
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <chrono>
 #include <functional>
+#include <thread>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,24 +14,26 @@
 #include <string.h>
 #include <unistd.h>
 
+#include <iomanip>
+
 using namespace std;
 
-//константы
+// константы
 #define PORT 12346
-#define IP_ADR ((in_addr_t)0x7F000001) //127.0.0.1
+#define IP_ADR ((in_addr_t)0x7F000001) // 127.0.0.1
 
-//структуры и объединения
+// структуры и объединения
 
 /* ARINC-429 */
 
 // BNR
-typedef union ARINC429_BNR_STRUCTURE
+typedef struct ARINC429_BNR_STRUCTURE
 {
-    unsigned short label : 8; //он же адрес
+    unsigned short label : 8; // он же адрес
     unsigned short SDI : 2;
 
-    //data
-    unsigned int higehst : 18; //65536 ??
+    // data
+    unsigned int higehst : 18; // 65536 ??
     unsigned short sign : 1;
 
     unsigned short SSM : 2;
@@ -38,42 +41,42 @@ typedef union ARINC429_BNR_STRUCTURE
 } arinc429_bnr;
 
 // BCD
-typedef union ARINC429_BCD_STRUCTURE
+typedef struct ARINC429_BCD_STRUCTURE
 {
-    unsigned short label : 8; //он же адрес
+    unsigned short label : 8; // он же адрес
     unsigned short SDI : 2;
-    unsigned short empt1 : 1; //не использующийся разряд(ы)
+    unsigned short empt1 : 1; // не использующийся разряд(ы)
 
-    //data
-    unsigned short sec : 6;   //32 ??
-    unsigned short min : 6;   //32 ??
-    unsigned short hrs : 5;   //16 ??
-    unsigned short empt2 : 1; //не использующийся разряд(ы)
+    // data
+    unsigned short sec : 6;   // 32 ??
+    unsigned short min : 6;   // 32 ??
+    unsigned short hrs : 5;   // 16 ??
+    unsigned short empt2 : 1; // не использующийся разряд(ы)
 
     unsigned short SSM : 2;
     unsigned short P : 1;
 } arinc429_bcd;
 
 // DISCRETE
-typedef union ARINC429_BCD_DISCRETE
+typedef struct ARINC429_DISCRETE_STRUCTURE
 {
-    unsigned short label : 8; //он же адрес
+    unsigned short label : 8; // он же адрес
     unsigned short SDI : 2;
 
-    //data
-    unsigned short prep_ZK : 1; //подготовка по ЗК
+    // data
+    unsigned short prep_ZK : 1; // подготовка по ЗК
     unsigned short control : 1;
     unsigned short navigation : 1;
-    unsigned short gyrocopmassing : 1; //0-ЗК, 1-ГК
-    unsigned short empt1 : 1;          //не использующийся разряд(ы)
+    unsigned short gyrocopmassing : 1; // 0-ЗК, 1-ГК
+    unsigned short empt1 : 1;          // не использующийся разряд(ы)
     unsigned short relaunch : 1;
     unsigned short prep_scale : 3;
-    unsigned short heat : 1;      //исправность обогрева
-    unsigned short termostat : 1; //термостатирование
-    unsigned short init_data : 1; //начальные данные 0-есть н.д., 1- нет н.д.
-    unsigned short H_abc : 1;     //0-есть, 1-нет
-    unsigned short boost : 1;     //готовность ускорения
-    unsigned short ready : 1;     //готовность
+    unsigned short heat : 1;      // исправность обогрева
+    unsigned short termostat : 1; // термостатирование
+    unsigned short init_data : 1; // начальные данные 0-есть н.д., 1- нет н.д.
+    unsigned short H_abc : 1;     // 0-есть, 1-нет
+    unsigned short boost : 1;     // готовность ускорения
+    unsigned short ready : 1;     // готовность
     unsigned short empt2 : 3;
 
     unsigned short SSM : 2;
@@ -82,46 +85,46 @@ typedef union ARINC429_BCD_DISCRETE
 
 /* MIL-1553B */
 
-//командное слово
-typedef union MIL1553_COMMAND_WORD_STRUCTURE
+// командное слово
+typedef struct MIL1553_COMMAND_WORD_STRUCTURE
 {
-    unsigned short sync_signal : 3; //снихро-сигнал
+    unsigned short sync_signal : 3; // снихро-сигнал
 
-    //data
-    unsigned short adress_OU : 5; //адрес ОУ
+    // data
+    unsigned short adress_OU : 5; // адрес ОУ
     unsigned short K : 1;
-    unsigned short subadres : 5; //подадрес/режим управления
-    unsigned short SD_num : 5;   // число СД/код команды
+    unsigned short subadres : 5; // подадрес/режим управления
+    unsigned short SD_num : 5;   //  число СД/код команды
 
     unsigned short P : 1;
 } mil1553_command;
 
-//слово данных
-typedef union MIL1553_DATA_WORD_STRUCTURE
+// слово данных
+typedef struct MIL1553_DATA_WORD_STRUCTURE
 {
-    unsigned short sync_signal : 3; //снихро-сигнал
+    unsigned short sync_signal : 3; // снихро-сигнал
 
-    //data
-    unsigned short data : 16; //данные
+    // data
+    unsigned short data : 16; // данные
 
     unsigned short P : 1;
 } mil1553_data;
 
-//ответное слово
-typedef union MIL1553_ANSWER_WORD_STRUCTURE
+// ответное слово
+typedef struct MIL1553_ANSWER_WORD_STRUCTURE
 {
-    unsigned short sync_signal : 3; //снихро-сигнал
+    unsigned short sync_signal : 3; // снихро-сигнал
 
-    //data
-    unsigned short adress_OU : 5; //адрес ОУ
+    // data
+    unsigned short adress_OU : 5; // адрес ОУ
     unsigned short msg_error : 1;
-    unsigned short OS_transf : 1; //передача ОС
-    unsigned short service : 1;   //запрос на обслуживание
+    unsigned short OS_transf : 1; // передача ОС
+    unsigned short service : 1;   // запрос на обслуживание
     unsigned short reserve : 3;
-    unsigned short group_cmd : 1; //принята групповая команда
-    unsigned short busy : 1;      //абонент занят
-    unsigned short defect : 1;    //неисправность абонента
-    unsigned short interface : 1; //принято управление интерфейсом
+    unsigned short group_cmd : 1; // принята групповая команда
+    unsigned short busy : 1;      // абонент занят
+    unsigned short defect : 1;    // неисправность абонента
+    unsigned short interface : 1; // принято управление интерфейсом
     unsigned short OU_defect : 1;
 
     unsigned short P : 1;
@@ -133,38 +136,90 @@ class Timer
 {
 public:
     Timer(){};
-    void add(std::chrono::milliseconds delay,
-             std::function<void()> callback);
+    void add(std::chrono::milliseconds delay, std::function<void()> callback)
+    {
+        auto  start = std::chrono::system_clock::now();
+        auto  current = std::chrono::system_clock::now();
+        while ((current - start) < delay) 
+        {
+            current = std::chrono::system_clock::now();
+        }
+        callback();
+    };
 };
 
-//глобальные переменные
+// функции
 
-//реализация
+void send_data(int socket, struct sockaddr* addr, char data) 
+{
+    char buf[4] = {data};
+    cout << "in func buf: " << sizeof(data) << endl;
+    cout << "in func addr: " << sizeof(&addr) << endl;
+    if (sendto(socket, &data, sizeof(data), 0, addr, sizeof(&addr)) == -1)
+		{
+			perror("sending error");
+		}
+};
+
+// глобальные переменные
+
+// реализация
 int main(int argc, char *argv[])
 {
     cout << "start\n";
 
+    arinc429_bcd bcd;
+    arinc429_bnr bnr;
+    arinc429_discrete discrete;
+    mil1553_data ml_data;
+    mil1553_command cmd;
+    mil1553_answer answer;
+
+    bcd.label = 200;
+    bcd.SDI = 1;
+    bcd.empt1 = 1;
+    bcd.empt2 = 1;
+    bcd.hrs = 29;
+    bcd.min = 40;
+    bcd.sec = 55;
+    bcd.SSM = 3;
+    bcd.P = 1;
+
+    bnr.label = 201;
+    bnr.SDI = 1;
+    bnr.higehst = 252;
+    bnr.sign = 1;
+    bnr.SSM = 3;
+    bnr.P = 1;
+
+    cout << "bcd: " << sizeof(bcd) << " byte" << endl;
+    cout << "bnr: " << sizeof(bnr) << " byte" << endl;
+    cout << "discrete: " << sizeof(discrete) << " byte" << endl;
+    cout << "ml_data: " << sizeof(ml_data) << " byte" << endl;
+    cout << "cmd: " << sizeof(cmd) << " byte" << endl;
+    cout << "answer: " << sizeof(answer) << " byte" << endl;
+
     struct sockaddr_in adr, oth;
     memset((char *)&adr, 0, sizeof(adr));
     adr.sin_family = AF_INET;
-    adr.sin_port = htons(PORT);          //Host TO Network Short
-    adr.sin_addr.s_addr = htonl(IP_ADR); //Host TO Network Long
+    adr.sin_port = htons(PORT);          // Host TO Network Short
+    adr.sin_addr.s_addr = htonl(IP_ADR); // Host TO Network Long
 
     int s, i, slen = sizeof(adr);
 
-    //создание соединения
+    // создание соединения
     if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) //IPPROTO_UDP
     {
         perror("connection error");
     }
 
-    //bind socket to port
+    // bind socket to port
     if (bind(s, (struct sockaddr *)&adr, slen) == -1)
     {
         perror("binding error");
     }
 
-    //data
+    // data
     mil1553_data mil_pack1;
     mil_pack1.sync_signal = 1;
     mil_pack1.data = 5;
@@ -172,9 +227,20 @@ int main(int argc, char *argv[])
 
     //char* pack_ptr = (char*)&mil_pack1;
 
-    //send data
-    char buf[20] = {char(*(char*)&mil_pack1)};
-    if (sendto(s, buf, 3, 0, (struct sockaddr *)&adr, sizeof(adr)) == -1)
+    // send data
+    
+
+    char buf[sizeof(bnr)];
+
+    memcpy(buf, &bnr, sizeof(buf));
+
+    cout << "in main buf: " << sizeof(buf) << endl;
+    cout << "in main: " << sizeof(adr) << endl;
+
+    struct sockaddr* sock_addr = (struct sockaddr *)&adr;
+    //send_data(s, sock_addr, res);
+
+    if (sendto(s, buf, sizeof(buf), 0, (struct sockaddr *)&adr, sizeof(adr)) == -1)
 		{
 			perror("sending error");
 		} 
