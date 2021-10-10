85174f1d73ec87e7415156d9081022c56de2652d
st1nkybom <akulakova29@gmail.com>
2021-10-10 16:39:02 +0300

    +

--- main.cpp
+++ main.cpp
@@ -27,118 +27,141 @@ using namespace std;
 
 // BNR
 #pragma pack(push,1)
-typedef struct ARINC429_BNR_STRUCTURE
-{
-    unsigned short label : 8; // он же адрес
-    unsigned short SDI : 2;
+union ARINC426_BNR_UNION {
+    struct ARINC429_BNR_STRUCTURE
+    {
+        unsigned short label : 8; // он же адрес
+        unsigned short SDI : 2;
 
-    // data
-    unsigned int higehst : 18; // 65536 ??
-    unsigned short sign : 1;
+        // data
+        unsigned int higehst : 18; // 65536 ??
+        unsigned short sign : 1;
 
-    unsigned short SSM : 2;
-    unsigned short P : 1;
-} arinc429_bnr;
+        unsigned short SSM : 2;
+        unsigned short P : 1;
+    };
+    unsigned int Word;
+}; 
 #pragma pack(pop)
 
 // BCD
 #pragma pack(push,1)
-typedef struct ARINC429_BCD_STRUCTURE
+union ARINC429_BCD_UNION
 {
-    unsigned short label : 8; // он же адрес
-    unsigned short SDI : 2;
-    unsigned short empt1 : 1; // не использующийся разряд(ы)
+    struct ARINC429_BCD_STRUCTURE
+    {
+        unsigned short label : 8; // он же адрес
+        unsigned short SDI : 2;
+        unsigned short empt1 : 1; // не использующийся разряд(ы)
 
-    // data
-    unsigned short sec : 6;   // 32 ??
-    unsigned short min : 6;   // 32 ??
-    unsigned short hrs : 5;   // 16 ??
-    unsigned short empt2 : 1; // не использующийся разряд(ы)
+        // data
+        unsigned short sec : 6;   // 32 ??
+        unsigned short min : 6;   // 32 ??
+        unsigned short hrs : 5;   // 16 ??
+        unsigned short empt2 : 1; // не использующийся разряд(ы)
 
-    unsigned short SSM : 2;
-    unsigned short P : 1;
-} arinc429_bcd;
+        unsigned short SSM : 2;
+        unsigned short P : 1;
+    } arinc429_bcd;
+    unsigned int Word;
+};
 #pragma pack(pop)
 
 // DISCRETE
 #pragma pack(push,1)
-typedef struct ARINC429_DISCRETE_STRUCTURE
+union ARINC429_DISCRETE_UNION
 {
-    unsigned short label : 8; // он же адрес
-    unsigned short SDI : 2;
+    struct ARINC429_DISCRETE_STRUCTURE
+    {
+        unsigned short label : 8; // он же адрес
+        unsigned short SDI : 2;
 
-    // data
-    unsigned short prep_ZK : 1; // подготовка по ЗК
-    unsigned short control : 1;
-    unsigned short navigation : 1;
-    unsigned short gyrocopmassing : 1; // 0-ЗК, 1-ГК
-    unsigned short empt1 : 1;          // не использующийся разряд(ы)
-    unsigned short relaunch : 1;
-    unsigned short prep_scale : 3;
-    unsigned short heat : 1;      // исправность обогрева
-    unsigned short termostat : 1; // термостатирование
-    unsigned short init_data : 1; // начальные данные 0-есть н.д., 1- нет н.д.
-    unsigned short H_abc : 1;     // 0-есть, 1-нет
-    unsigned short boost : 1;     // готовность ускорения
-    unsigned short ready : 1;     // готовность
-    unsigned short empt2 : 3;
+        // data
+        unsigned short prep_ZK : 1; // подготовка по ЗК
+        unsigned short control : 1;
+        unsigned short navigation : 1;
+        unsigned short gyrocopmassing : 1; // 0-ЗК, 1-ГК
+        unsigned short empt1 : 1;          // не использующийся разряд(ы)
+        unsigned short relaunch : 1;
+        unsigned short prep_scale : 3;
+        unsigned short heat : 1;      // исправность обогрева
+        unsigned short termostat : 1; // термостатирование
+        unsigned short init_data : 1; // начальные данные 0-есть н.д., 1- нет н.д.
+        unsigned short H_abc : 1;     // 0-есть, 1-нет
+        unsigned short boost : 1;     // готовность ускорения
+        unsigned short ready : 1;     // готовность
+        unsigned short empt2 : 3;
 
-    unsigned short SSM : 2;
-    unsigned short P : 1;
-} arinc429_discrete;
+        unsigned short SSM : 2;
+        unsigned short P : 1;
+    };
+    unsigned int Word;
+};
 #pragma pack(pop)
 
 /* MIL-1553B */
 
 // командное слово
 #pragma pack(push,1)
-typedef struct MIL1553_COMMAND_WORD_STRUCTURE
+union MIL1553_COMMAND_WORD_UNION 
 {
-    unsigned short sync_signal : 3; // снихро-сигнал
+    struct MIL1553_COMMAND_WORD_STRUCTURE
+    {
+        unsigned short sync_signal : 3; // снихро-сигнал
 
-    // data
-    unsigned short adress_OU : 5; // адрес ОУ
-    unsigned short K : 1;
-    unsigned short subadres : 5; // подадрес/режим управления
-    unsigned short SD_num : 5;   //  число СД/код команды
+        // data
+        unsigned short adress_OU : 5; // адрес ОУ
+        unsigned short K : 1;
+        unsigned short subadres : 5; // подадрес/режим управления
+        unsigned short SD_num : 5;   //  число СД/код команды
 
-    unsigned short P : 1;
-} mil1553_command;
+        unsigned short P : 1;
+    };
+    unsigned int Word;
+};
 #pragma pack(pop)
 
 // слово данных
 #pragma pack(push,1)
-typedef struct MIL1553_DATA_WORD_STRUCTURE
+union MIL1553_DATA_WORD_STRUCTURE_UNION
 {
-    unsigned short sync_signal : 3; // снихро-сигнал
+    struct MIL1553_DATA_WORD_STRUCTURE
+    {
+        unsigned short sync_signal : 3; // снихро-сигнал
 
-    // data
-    unsigned short data : 16; // данные
+        // data
+        unsigned short data : 16; // данные
 
-    unsigned short P : 1;
-} mil1553_data;
+        unsigned short P : 1;
+    };
+    unsigned int Word;
+};
 #pragma pack(pop)
 
 // ответное слово
 #pragma pack(push, 1)
-typedef struct MIL1553_ANSWER_WORD_STRUCTURE
+union MIL1553_ANSWER_WORD_UNION
 {
-    unsigned short sync_signal : 3; // снихро-сигнал
+    struct MIL1553_ANSWER_WORD_STRUCTURE
+    {
+        unsigned short sync_signal : 3; // снихро-сигнал
 
-    // data
-    unsigned short adress_OU : 5; // адрес ОУ
-    unsigned short msg_error : 1;
-    unsigned short OS_transf : 1; // передача ОС
-    unsigned short service : 1;   // запрос на обслуживание
-    unsigned short reserve : 3;
-    unsigned short group_cmd : 1; // принята групповая команда
-    unsigned short busy : 1;      // абонент занят
-    unsigned short defect : 1;    // неисправность абонента
-    unsigned short interface : 1; // принято управление интерфейсом
-    unsigned short OU_defect : 1;
+        // data
+        unsigned short adress_OU : 5; // адрес ОУ
+        unsigned short msg_error : 1;
+        unsigned short OS_transf : 1; // передача ОС
+        unsigned short service : 1;   // запрос на обслуживание
+        unsigned short reserve : 3;
+        unsigned short group_cmd : 1; // принята групповая команда
+        unsigned short busy : 1;      // абонент занят
+        unsigned short defect : 1;    // неисправность абонента
+        unsigned short interface : 1; // принято управление интерфейсом
+        unsigned short OU_defect : 1;
 
-    unsigned short P : 1;
-} mil1553_answer;
+        unsigned short P : 1;
+    };
+    unsigned int Word;
+};
 #pragma pack(pop)
 
 /* таймер */
@@ -200,41 +223,41 @@ int main(int argc, char *argv[])
     }
 
     // data
-    arinc429_bcd bcd;
-    arinc429_bnr bnr;
-    arinc429_discrete discrete;
-    mil1553_data ml_data;
-    mil1553_command cmd;
-    mil1553_answer answer;
+    ARINC429_BCD_UNION bcd;
+    ARINC426_BNR_UNION bnr;
 
-    bcd.label = 200;
-    bcd.SDI = 1;
-    bcd.empt1 = 1;
-    bcd.empt2 = 1;
-    bcd.hrs = 29;
-    bcd.min = 40;
-    bcd.sec = 55;
-    bcd.SSM = 3;
-    bcd.P = 1;
+    bcd.arinc429_bcd.label = 200;
+    bcd.arinc429_bcd.SDI = 1;
+    bcd.arinc429_bcd.empt1 = 1;
+    bcd.arinc429_bcd.empt2 = 1;
+    bcd.arinc429_bcd.hrs = 29;
+    bcd.arinc429_bcd.min = 40;
+    bcd.arinc429_bcd.sec = 55;
+    bcd.arinc429_bcd.SSM = 3;
+    bcd.arinc429_bcd.P = 1;
 
+/*
     bnr.label = 201;
     bnr.SDI = 1;
     bnr.higehst = 252;
     bnr.sign = 1;
     bnr.SSM = 3;
     bnr.P = 1;
+*/
 
     cout << "arinc429 bcd: " << sizeof(bcd) << " byte" << endl;
     cout << "arinc429 bnr: " << sizeof(bnr) << " byte" << endl;
+    /*
     cout << "arinc429 discrete: " << sizeof(discrete) << " byte" << endl;
     cout << "mil1553 data: " << sizeof(ml_data) << " byte" << endl;
     cout << "mil1553 cmd: " << sizeof(cmd) << " byte" << endl;
     cout << "mil1553 answer: " << sizeof(answer) << " byte" << endl;
+    */
 
     // send data
     Timer timer;
     bool a = 1;
     while (a) {
-        timer.add(std::chrono::seconds(1), send_data, s, adr, &bnr, sizeof(bnr));
+        timer.add(std::chrono::seconds(1), send_data, s, adr, &bcd.Word, sizeof(bcd.Word));
     }
 }
